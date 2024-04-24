f06c446a3b6a1c77963cf291bab3a12b1474cb43
Reynir Björnsson <reynir@reynir.dk>
2024-04-24 08:56:13 +0200

    Document why send_application_data is None

--- lib/engine.mli
+++ lib/engine.mli
@@ -163,9 +163,10 @@ val handle_tls           : state -> string -> ret
     is a handshake in progress or scheduled. *)
 val handshake_in_progress : state -> bool
 
-(** [send_application_data tls outs] is [(tls' * out) option] where
+(** [send_application_data tls outs] is [Some (tls', out)] where
     [tls'] is the new tls state, and [out] the cstruct to send over the
-    wire (encrypted [outs]). *)
+    wire (encrypted [outs]) when the TLS session is ready. When the TLS
+    session is not ready it is [None]. *)
 val send_application_data : state -> string list -> (state * string) option
 
 (** [send_close_notify tls] is [tls' * out] where [tls'] is the new
